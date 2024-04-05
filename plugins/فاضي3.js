c457c4223de35a5c07971fd24e91f1e24219b4db
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-05 22:59:47 +0300

    Update فاضي3.js

--- plugins/فاضي3.js
+++ plugins/فاضي3.js
@@ -1,1 +1,26 @@
-هه
+const dir = [
+  'https://i.pinimg.com/originals/3a/78/8d/3a788d8410d80a79dd64ab889c3692f4.jpg', 
+  'https://cdn4.iporntv.net/videos/thumbsl/a6/6c/f2/a66cf219c251809b93c627a0fd849bea/a66cf219c251809b93c627a0fd849bea.4.jpg', 
+  'https://ei.phncdn.com/videos/202207/30/412713271/original/(m=eaAaGwObaaaa)(mh=uTSrMkdMugt1YO_T)11.jpg', 
+  'https://atto.scrolller.com/astolfo-getting-fucked-hard-zheng-3e30u9ptox-540x760.jpg', 
+  'https://telegra.ph/file/a632be206ddd0b1d11742.jpg', 
+  'https://telegra.ph/file/f087dd0057dffb9236107.jpg', 
+  'https://telegra.ph/file/ff7fa1ed4b3974221191d.jpg', 
+  'https://telegra.ph/file/4c07ba64e40dd1e588923.jpg', 
+  'https://cdn77-pic.xvideos-cdn.com/videos/thumbs169ll/f8/d6/10/f8d610c59ad4e0bbaad1d474d93f1104-2/f8d610c59ad4e0bbaad1d474d93f1104.29.jpg', 
+  'https://cdn.donmai.us/original/5c/0b/5c0bc38dfea988c19ac296d0a09375cc.jpg', 
+  'https://telegra.ph/file/c7f4d741dbdf4e3411950.jpg', 
+  'https://cdn.donmai.us/original/5c/0b/5c0bc38dfea988c19ac296d0a09375cc.jpg', 
+  'https://img-l3.xnxx-cdn.com/videos/thumbs169lll/a4/05/0a/a4050ab8e28450a8e57840ec4607b43c/a4050ab8e28450a8e57840ec4607b43c.7.jpg', 
+  'https://di.phncdn.com/videos/202205/27/408861831/original/(m=eaf8Ggaaaa)', 
+  'https://www.pantypit.com/wordpress/wp-content/uploads/2014/05/wetwhitepantyshower2.jpg', 
+  'https://di.phncdn.com/videos/201911/04/259329852/original/(m=eaAaGwObaaaa)(mh=9JgBkg-AoNchh5jE)14.jpg', 
+  'https://di.phncdn.com/videos/201911/04/259329852/original/(m=eaAaGwObaaaa)(mh=9JgBkg-AoNchh5jE)14.jpg', 
+];
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '*انمي بنات نيكو💜💫*', m)
+}
+handler.help = ['dado']
+handler.tags = ['game']
+handler.command = ['كبنت', 'dados'] 
+export default handler
