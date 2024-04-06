8613e9b7bf29dc9a674a961100e26395881c141a
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-06 14:27:38 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -1,10 +1,12 @@
+// كود تم نشره بواسطه obito
+// تابع لقناه https://whatsapp.com/channel/0029VaSQX1TI1rcbxtQZ5518
 let handler = async (m, { conn }) => {
 let done = '🩶'; 
        m.react(done);
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
-    let taguser = '@اهلين' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*⎔↞┃ هل تحتاج على مساعده اكتب الاوامر┃*`;
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*⎔↞┃اهلين اكتب الاوامر للمساعدة┃*`;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/5cbf1d6fcd25dea5b80cb.jpg', 'image.jpg', message, m);
 };
