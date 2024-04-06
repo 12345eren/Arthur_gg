dbe40c1f6dd4d32398780feedc43fa1559a2742e
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-06 07:26:19 +0300

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -2,7 +2,7 @@ function handler(m) {
   let data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
-handler.alias = ['المطور']
-handler.command = /^(المالك|المطور)$/i
+handler.alias = ['صاحب البوت']
+handler.command = /^(المالك|owner)$/i
 
 export default handler
