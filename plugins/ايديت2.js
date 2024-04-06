f0dd041005a2c07fc88cc37b769e15d48703bb68
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-06 14:38:22 +0300

    Update ايديت2.js

--- plugins/ايديت2.js
+++ plugins/ايديت2.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 
-    await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: `*┃الفيديو يوجد فيه موسيقي┃↞⎔*` }, { quoted: m })
+    await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: `*⎔↞┃الفيديو يوجد فيه موسيقي❗️┃*` }, { quoted: m })
 }
 
-handler.help = ['ايديت']
-handler.tags = ['animestory']
+handler.help = ['animestory']
+handler.tags = ['ادت']
 handler.command = /^animestory$/i
 
 
