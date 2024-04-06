40627dd38b36335f769dfe9f7f85f466ad5b329c
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-06 14:20:07 +0300

    Update علم.js

--- plugins/علم.js
+++ plugins/علم.js
@@ -1,10 +1,11 @@
+//🧧🗿🗿🗿🗿🗿
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        conn.reply(m.chat, '⚡️┃لم يتم الاجابة علي السؤال بعد┃⚡️ ❯', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://raw.githubusercontent.com/Hjfjckb/Kurosaki/main/Kurosaki.json')).json()
@@ -19,7 +20,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         await conn.sendFile(m.chat, json.question, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ 💧┃انتهي الوقت┃💧❯\n❐↞┃الاجـابـة✓↞ ${json.response}*┇`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ 💧┃انتهي الوقت┃💧❯\n⎔↞┃الاجـابـة✓↞ ${json.response}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
