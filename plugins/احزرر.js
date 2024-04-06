10df5f54c93a1919e8bf9e9cfc16c6264db31818
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-06 13:48:56 +0300

    Update احزرر.js

--- plugins/احزرر.js
+++ plugins/احزرر.js
@@ -10,16 +10,16 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/98d564d49cbf9b539fee19f744de7b26/raw/f2a3e68bbcdd2b06f9dbd5f30d70b9fda42fec14/guessflag')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*${command.toUpperCase()}*
-  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  ⎔↞┃الـوقـت⏱️↞ *${(timeout / 1000).toFixed(2)} ┃
   *استخدم .انسحب للأنسحاب*
-  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『』
+  ⎔↞┃الـجـائـزة🎖↞ ${poin} نقاط┃
+┃𝑬𝑹𝑬𝑵_ 𝑩𝑶𝑻┃↞⎔
      `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ 💧┇انتهي الوقت┇💧❯\n❐↞┇الاجـابـة✓↞ ${json.name}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
