803d4ba067efd492162212aff7922818c154fa25
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-07 04:36:23 +0300

    Update and rename anonymous_chat.js to بينترست.js

---
+++ plugins/بينترست.js
@@ -0,0 +1,88 @@
+cmd({
+
+pattern: "pint",
+
+alias :["بينت","بينترست"],
+
+react: "📥",
+
+desc: "Downloads image from pinterest.",
+
+category: "downloader",
+
+filename: __filename,
+
+use: '<text|image name>',
+
+},
+
+async(Void, citel, text) => {
+
+if (!text) return citel.reply("*֎╎اكـتـب عـنـوان للـبـحـث*") && Void.sendMessage(citel.chat, {
+
+react: {
+
+text: '❌',
+
+key: citel.key
+
+}
+
+})
+
+try {
+
+anu = await pinterest(text)
+
+result = anu[Math.floor(Math.random() * anu.length)]
+
+let buttonMessage = {
+
+image: {
+
+url: result
+
+},
+
+caption: ` `,
+
+footer: tlang().footer,
+
+headerType: 4,
+
+contextInfo: {
+
+externalAdReply: {
+
+title: `*┃𝑬𝑹𝑬𝑵_ 𝑩𝑶𝑻┃↞⎔*`,
+
+body: `${Config.ownername}`,
+
+thumbnail: log0,
+
+mediaType: 2,
+
+mediaUrl: ``,
+
+sourceUrl: ``
+
+}
+
+}
+
+}
+
+return Void.sendMessage(citel.chat, buttonMessage, {
+
+quoted: citel
+
+})
+
+} catch (e) {
+
+console.log(e)
+
+}
+
+})
+
