dc9019d9061320ceb91da6df2d117bca44e55f35
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-08 13:16:49 +0300

    Update تيك.js

--- plugins/تيك.js
+++ plugins/تيك.js
@@ -6,14 +6,14 @@ global.rpg = {
     if (isNaN(level)) return { name: '', level: '' }
     
     const role = [
-      { name: "رضيع", level: 0 }, { name: "مبتدئ", level: 4 }, 
-      { name: "ماهر", level: 8 }, { name: "القادر علي التحدي", level: 12 }, 
-      { name: "يتقن", level: 16 }, { name: "المطرشم", level: 20 }, 
+      { name: "ضعيف", level: 0 }, { name: "مبتدئ", level: 4 }, 
+      { name: "مقاتل", level: 8 }, { name: "القادر علي التحدي", level: 12 }, 
+      { name: "محارب", level: 16 }, { name: "المطرشم", level: 20 }, 
       { name: "بطل", level: 24 }, { name: "العالمي", level: 28 }, 
       { name: "اسطوره", level: 32 }, { name: "خرافة", level: 36 },
       { name: "ساحر", level: 48 }, { name: "الساحر القوي", level: 52 }, 
       { name: "حكيم", level: 56 }, { name: "فاحر", level: 60 }, 
-      { name: "عم الكل", level: 100 }
+      { name: "صياد", level: 100 }
     ];
 
     return role.reverse().find(role => level >= role.level)
