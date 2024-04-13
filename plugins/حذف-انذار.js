c051254aa8322640b3269e194b530de1c4f7b7bd
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-13 04:55:24 +0300

    Update حذف-انذار.js

--- plugins/حذف-انذار.js
+++ plugins/حذف-انذار.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `*مــنشـن الـشـخص !*`
+        if (!who) throw `*مــنشـن الـشـخص┃🚫┃*`
         if (!(who in global.db.data.users)) throw `*المستخدم غير موجود في قاعدة البيانات الخاصة بي*`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
@@ -14,13 +14,13 @@ let handler = async (m, { conn, args, groupMetadata}) => {
 ▢ عدد الانذارات الحالي: *${warn - 1}*`)
          m.reply(`✳️ قام أحد المسؤولين بتخفيض تحذيره ، والآن لديك *${warn - 1}*`, who)
          } else if (warn == 0) {
-            m.reply('*المستخدم ليس لديه انذارات*')
+            m.reply('*⎔↞┃المستخدم ليس لديه انذارات*')
         }
 
 }
 handler.help = ['delwarn @user']
 handler.tags = ['group']
-handler.command = ['حذف_انذار', 'حذف_تحذير'] 
+handler.command = ['الغاء-انذار', 'الغاء_تحذير'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
