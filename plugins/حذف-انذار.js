80c47343d3dafcca669c6d580c27a6890cae699c
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-13 04:29:04 +0300

    Update حذف-انذار.js

--- plugins/حذف-انذار.js
+++ plugins/حذف-انذار.js
@@ -1,30 +1,28 @@
-cmd({
 
-pattern: "حذف-انذار",
-
-alias: ["اعفاء"],
-
-desc: "Deletes all previously given warns of quoted user.",
-
-category: "group",
-
-filename: __filename,
-
-use: '<quote|reply|number>',
-
-},
-
-async(Void, citel, text,{isCreator}) => {
-
-if (!isAdmins) return citel.reply(tlang().admin)
-
-if (!citel.quoted) return citel.reply('*֎╎مـنـشـن عـلـي شـخـص*')
-
-await warndb.deleteOne({ id: citel.quoted.sender.split('@')[0] + 'warn' });
-
-return citel.reply('*֎╎تـم حـذف انـذار*')
+let handler = async (m, { conn, args, groupMetadata}) => {
+        let who
+        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+        else who = m.chat
+        if (!who) throw `*مــنشـن الـشـخص !*`
+        if (!(who in global.db.data.users)) throw `*المستخدم غير موجود في قاعدة البيانات الخاصة بي*`
+       let warn = global.db.data.users[who].warn
+       if (warn > 0) {
+         global.db.data.users[who].warn -= 1
+         m.reply(`⚠️ *حذف الانذار*
+         
+▢الانذار : *-1*
+▢ عدد الانذارات الحالي: *${warn - 1}*`)
+         m.reply(`✳️ قام أحد المسؤولين بتخفيض تحذيره ، والآن لديك *${warn - 1}*`, who)
+         } else if (warn == 0) {
+            m.reply('*المستخدم ليس لديه انذارات*')
+        }
 
 }
+handler.help = ['delwarn @user']
+handler.tags = ['group']
+handler.command = ['حذف_انذار', 'حذف_تحذير'] 
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
 
-)
-
+export default handler
