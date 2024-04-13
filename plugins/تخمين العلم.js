5e6d77c4329a0a686610a4511844ab37604e7875
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-13 05:01:20 +0300

    Update تخمين العلم.js

--- plugins/تخمين العلم.js
+++ plugins/تخمين العلم.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 conn.reply(m.chat,`**\n\n*“${pickRandom(global.verdaad)}”*\n\n**`, m)
 }
-handler.help = ['ايمو']
+handler.help = ['ايموجي']
 handler.tags = ['fun']
-handler.command = /^ايمو/i
+handler.command = /^ايموجي/i
 export default handler
 function pickRandom(list) {
 return list[Math.floor(list.length * Math.random())]
