1efb3dedee52bcbbdbde13e3f884e13173357dd4
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-13 22:27:38 +0300

    Update باركود.js

--- plugins/باركود.js
+++ plugins/باركود.js
@@ -1,6 +1,6 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*اكتب كلام الي تبغاه يحول الى باركود 🧸*`
+if (!text) throw `*اكتب كلام الي تبغاه يحول الى باركود✓┃🚫┃ *`
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(BY:HODA_AND_MAHDI', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
