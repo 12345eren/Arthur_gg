6537f9e9f52c01791c2803ad5d3ef1b90a0c5df7
12345eren <159772923+12345eren@users.noreply.github.com>
2024-04-13 10:34:23 +0300

    Update JOHAN-profile.js

--- plugins/JOHAN-profile.js
+++ plugins/JOHAN-profile.js
@@ -10,18 +10,16 @@ let handler = async (m, { conn }) => {
     let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let str = `
     *⎔⋅• ┗╼╼╃✦⊰💧⊱✦╄╼╾┛ •⋅⎔*
-*⎔↞┃ الاسم🀄 ↞* ${username} 
-*⎔↞┃ اسم🪧 ↞* ${registered ? name : ''}
-*⎔↞┃ منشن⚡️↞* @${who.replace(/@.+/, '')}
-*⎔↞┃ التصنيف 🧮 ↞ ${role}*
-*⎔↞┃ المستوى 📊 ↞ ${level}*
-*⎔↞┃ رقم🎴 ↞* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-*⎔↞┃ رابط📮 ↞* https://wa.me/${who.split`@`[0]}
-*⎔↞┃ عمر🔍 ↞* ${registered ? age : ''} Tahun
-*⎔↞┃ شري🍁 ↞*  ${pasangan ? `@${pasangan.split("@")[0]}` : `Tidak Punya`}
-*⎔↞┃ طلب📠 ↞* ${registered ? 'Terdaftar': 'Tidak'}
-*⎔↞┃ مميز💧↞* ${premium ? "Aktif" :"Tidak"}
-*⎔↞┃ وقت مميز⏱️ ↞* 
+*⎔↞┃الاسم🀄 ↞* ${username} 
+*⎔↞┃اسم🎖 ↞* ${registered ? name : ''}
+*⎔↞┃منشن🎴 ↞* @${who.replace(/@.+/, '')}
+*⎔↞┃رقم📮 ↞* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+*⎔↞┃رابط⚡️ ↞* https://wa.me/${who.split`@`[0]}
+*⎔↞┃عمر🔍↞* ${registered ? age : ''} Tahun
+*⎔↞┃شريك🍁 ↞*  ${pasangan ? `@${pasangan.split("@")[0]}` : `Tidak Punya`}
+*⎔↞┃طلب📠 ↞* ${registered ? 'Terdaftar': 'Tidak'}
+*⎔↞┃مميز📜 ↞* ${premium ? "Aktif" :"Tidak"}
+*⎔↞┃وقت مميز ↞⏱️* 
 ${clockString(user.premiumTime)}
 *⎔⋅• ┗╼╼╃✦⊰💧⊱✦╄╼╾┛ •⋅⎔*
 `.trim()
